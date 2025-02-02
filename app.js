f7a50d3f6a95b609498fe80e1f44907a6c7ed334
Muhammad Yousuf <152775949+MuhammadYousuf88@users.noreply.github.com>
2025-02-01 22:57:15 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
-
-/*
 function values(x){
 	document.getElementById('userInput').value += x;
 }
+
+function erase(){
+	document.getElementById('userInput').value = '';
+}
+
+function del(){
+	var del1 = document.getElementById('userInput');
+	del1.value = del1.value.slice(0,-1)
+}
+/*
+var arr = ["+" , "-" , "*" , "/"];
+console.log(arr)
+
+var num1 = "2+3-";
+var num2 = "+";
+
+var last = num1[num1 - 1];
+
+
+
+
+console.log(last)
+console.log(num1 += num2)
 */
