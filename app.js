d1e6226a996b629bae689486fc4d4848ecebb864
Muhammad Yousuf <152775949+MuhammadYousuf88@users.noreply.github.com>
2025-02-06 23:30:05 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,74 +1,3 @@
-/*
-
-var firstNum = '';
-var lastNum = '';
-
-function values(x){
-	var inputNum = document.getElementById('userInput');
-	inputNum.value += x;
-	firstNum += inputNum;
-
-}
-
-function erase(){
-	document.getElementById('userInput').value = '';
-}
-
-function del(){
-	var del1 = document.getElementById('userInput');
-	del1.value = del1.value.slice(0,-1)
-}
-
-function cal(){
-	var finalValue = document.getElementById('userInput');
-	finalValue.value = eval(finalValue.value)
-}
-
-*/
-
-/*
-var arr = ["+" , "-" , "*" , "/"];
-console.log(arr)
-
-var num1 = "2+3-";
-var num2 = "+";
-
-var last = num1[num1 - 1];
-
-
-
-
-console.log(last)
-console.log(num1 += num2)
-*/
-
-
-
-
-/*
-function addNumber() {
-    // Get the value from the input field
-    const userInput = parseFloat(document.getElementById('numInput').value);
-
-    // Check if the input is a valid number
-    if (isNaN(userInput)) {
-        alert("Please enter a valid number.");
-        return;
-    }
-
-    // Pre-defined number to add to the user input (e.g., 10)
-    const predefinedNumber = 10;
-
-    // Add the user input to the predefined number
-    const sum = userInput + predefinedNumber;
-
-    // Display the result
-    document.getElementById('result').textContent = sum;
-}
-
-*/
-
-
 
 var currentInput = "";
 var currentOperation = "";
@@ -76,7 +5,16 @@ var previousInput = "";
 
 function numbers(value){
 	currentInput += value;
-	document.getElementById("screen").value = currentInput
+	document.getElementById("screen").value = currentInput;
+
+	return;
+}
+
+function del(){
+	var currentInput = document.getElementById('screen');
+	currentInput.value = currentInput.value.slice(0,-1);
+
+	return;
 }
 
 function operationSign(value){
@@ -84,6 +22,8 @@ function operationSign(value){
 	previousInput = currentInput;
 	currentInput = "";
 	document.getElementById("screen").value = previousInput + currentOperation + currentInput;
+	
+	return;
 }
 
 function sum(){
@@ -108,6 +48,8 @@ function sum(){
 	currentInput = result.toString();
 	previousInput = '';
 	currentOperation = '';
+
+	return;
 }
 
 function clearScreen(){
@@ -115,6 +57,23 @@ function clearScreen(){
 	currentOperation = "";
 	previousInput = "";
 	document.getElementById("screen").value = "";
+
+	return;
 }
 
 
+/*
+var previousInput ="5+5-";
+var currentOperation = "/";
+var changeSign = previousInput[previousInput.length - 1];
+
+var allSigns = ["+" , "-" , "*" ,"/"];
+if(allSigns.indexOf(changeSign) !== -1 && allSigns.indexOf(currentOperation) !== -1){
+	previousInput = previousInput.slice(0,-1) +currentOperation;
+}
+else{
+	previousInput += currentOperation;
+}
+
+console.log(previousInput)
+*/
